926bfee851e94f65364488e6cd52f9bfd5a1d4b1
JAICHANGPARK <aristojeff@gmail.com>
2026-01-29 18:54:12 +0900

    Update hotel_booking_start_page.dart

--- lib/ep2501_hotel_booking_app/ui/hotel_booking_start_page.dart
+++ lib/ep2501_hotel_booking_app/ui/hotel_booking_start_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class HotelBookingStartPage extends StatefulWidget {
   const HotelBookingStartPage({super.key});
 
@@ -14,7 +13,12 @@ class _HotelBookingStartPageState extends State<HotelBookingStartPage> {
     return Scaffold(
       body: Stack(
         children: [
-          
+          Positioned.fill(
+            child: Image.network(
+              "https://pixabay.com/ko/images/download/house-7405403_1920.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
         ],
       ),
     );
