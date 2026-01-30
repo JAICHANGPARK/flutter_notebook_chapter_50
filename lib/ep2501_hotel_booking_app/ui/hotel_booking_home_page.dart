9517134a6425cbdc621fe11918b060684bb9cc2a
JAICHANGPARK <aristojeff@gmail.com>
2026-01-30 17:22:14 +0900

    Create hotel_booking_home_page.dart

---
+++ lib/ep2501_hotel_booking_app/ui/hotel_booking_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HotelBookingHomePage extends StatefulWidget {
+  const HotelBookingHomePage({super.key});
+
+  @override
+  State<HotelBookingHomePage> createState() => _HotelBookingHomePageState();
+}
+
+class _HotelBookingHomePageState extends State<HotelBookingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
