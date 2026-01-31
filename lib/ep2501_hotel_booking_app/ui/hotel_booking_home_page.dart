94e4005de68f60caca80ea6151d506abf6cfd89e
JAICHANGPARK <aristojeff@gmail.com>
2026-01-31 10:03:28 +0900

    Update hotel_booking_home_page.dart

--- lib/ep2501_hotel_booking_app/ui/hotel_booking_home_page.dart
+++ lib/ep2501_hotel_booking_app/ui/hotel_booking_home_page.dart
@@ -56,7 +56,9 @@ class _HotelBookingHomePageState extends State<HotelBookingHomePage> {
                       shape: StadiumBorder(),
                       color: Color.fromRGBO(248, 250, 251, 1),
                     ),
-                    child: Row(children: []),
+                    child: Row(children: [
+                      HugeIcon(icon: HugeIcons.strokeRoundedSearch01),
+                    ]),
                   ),
                 ],
               ),
