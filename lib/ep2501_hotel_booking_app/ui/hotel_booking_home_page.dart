f49d25a3917b42be0bbba1f9ad711c7cdb09d4b4
JAICHANGPARK <aristojeff@gmail.com>
2026-02-02 18:11:45 +0900

    Update hotel_booking_home_page.dart

--- lib/ep2501_hotel_booking_app/ui/hotel_booking_home_page.dart
+++ lib/ep2501_hotel_booking_app/ui/hotel_booking_home_page.dart
@@ -312,22 +312,19 @@ class _HotelBookingHomePageState extends State<HotelBookingHomePage> {
                                   height: 32,
                                   child: Row(
                                     children: [
-                                      Container(
-                                        height: 32,
-                                        child: Stack(
-                                          children: List.generate(3, (idx) {
-                                            return Positioned(
-                                              left: 16.0 * idx,
-                                              bottom: 0,
-                                              top: 0,
-                                              child: CircleAvatar(
-
-                                                backgroundColor: Colors.blue,
-                                              ),
-                                            );
-                                          }),
-                                        ),
+                                      Stack(
+                                        children: List.generate(3, (idx) {
+                                          return Positioned(
+                                            left: 16.0 * idx,
+                                            bottom: 0,
+                                            top: 0,
+                                            child: CircleAvatar(
+                                              backgroundColor: Colors.blue,
+                                            ),
+                                          );
+                                        }),
                                       ),
+                                      Text("")
                                     ],
                                   ),
                                 ),
