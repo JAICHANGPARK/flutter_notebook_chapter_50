0d8b510f105e23bfc095eee7ab34e982bd870905
JAICHANGPARK <aristojeff@gmail.com>
2026-02-06 20:37:58 +0900

    Update delivery_main_page.dart

--- lib/ep2508_delivery_app/ui/delivery_main_page.dart
+++ lib/ep2508_delivery_app/ui/delivery_main_page.dart
@@ -11,7 +11,7 @@ class _DeliveryMainPageState extends State<DeliveryMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SafeArea(child: Column(children: []), bottom: false),
     );
   }
 }
