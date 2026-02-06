2f8eda17c30ea22381bc3b5b15274f965dc57e39
JAICHANGPARK <aristojeff@gmail.com>
2026-02-06 20:35:09 +0900

    Update delivery_app.dart

--- lib/ep2508_delivery_app/delivery_app.dart
+++ lib/ep2508_delivery_app/delivery_app.dart
@@ -5,6 +5,8 @@ class DeliveryApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: ,
+    );
   }
 }
