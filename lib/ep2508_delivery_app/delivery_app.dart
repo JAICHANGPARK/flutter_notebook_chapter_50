7b8131cf656dd65f2c0d547a5543dfcf61203a4f
JAICHANGPARK <aristojeff@gmail.com>
2026-02-06 20:36:17 +0900

    Update delivery_app.dart

--- lib/ep2508_delivery_app/delivery_app.dart
+++ lib/ep2508_delivery_app/delivery_app.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'ui/delivery_main_page.dart';
+
 class DeliveryApp extends StatelessWidget {
   const DeliveryApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ,
+      home: DeliveryMainPage(),
     );
   }
 }
