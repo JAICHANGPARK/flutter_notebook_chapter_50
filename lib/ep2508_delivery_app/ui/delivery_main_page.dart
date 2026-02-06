88dd9530abfa0a4001608a197fcf8ca905f584ed
JAICHANGPARK <aristojeff@gmail.com>
2026-02-06 20:35:41 +0900

    Update delivery_main_page.dart

---
+++ lib/ep2508_delivery_app/ui/delivery_main_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DeliveryMainPage extends StatefulWidget {
+  const DeliveryMainPage({super.key});
+
+  @override
+  State<DeliveryMainPage> createState() => _DeliveryMainPageState();
+}
+
+class _DeliveryMainPageState extends State<DeliveryMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
