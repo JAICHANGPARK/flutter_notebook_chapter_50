8749d8531bf8cc860e08fcdf31e777457c323bef
JAICHANGPARK <aristojeff@gmail.com>
2026-02-07 10:13:40 +0900

    Update delivery_main_page.dart

--- lib/ep2508_delivery_app/ui/delivery_main_page.dart
+++ lib/ep2508_delivery_app/ui/delivery_main_page.dart
@@ -66,7 +66,7 @@ class _DeliveryMainPageState extends State<DeliveryMainPage> {
                         decoration: InputDecoration(
                           hintText: "Search package",
                           border: .none,
-                          hintStyle: TextStyle(fontSize: 16, fontWeight: .bold),
+                          hintStyle: TextStyle(fontSize: 18, fontWeight: .bold),
                         ),
                       ),
                     ),
