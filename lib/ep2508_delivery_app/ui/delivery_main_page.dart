5e85347a8719d9585e44b0d2a2ad47f73613e41f
JAICHANGPARK <aristojeff@gmail.com>
2026-02-07 10:15:02 +0900

    Update delivery_main_page.dart

--- lib/ep2508_delivery_app/ui/delivery_main_page.dart
+++ lib/ep2508_delivery_app/ui/delivery_main_page.dart
@@ -15,91 +15,99 @@ class _DeliveryMainPageState extends State<DeliveryMainPage> {
       body: SafeArea(
         bottom: false,
         child: Padding(
-            padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
-            child: Column(
-              spacing: 20,
-              children: [
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+          child: Column(
+            spacing: 20,
+            children: [
               Row(
-              spacing: 9,
-              children: [
-                Container(
-                  height: 52,
-                  width: 52,
-                  decoration: ShapeDecoration(
-                    color: .fromRGBO(53, 223, 104, 1),
-                    shape: RoundedSuperellipseBorder(
-                      borderRadius: .circular(12),
-                    ),
-                  ),
-                ),
-                Text(
-                  "Hello, Dream!",
-                  style: TextStyle(fontSize: 22, fontWeight: .bold),
-                ),
-                Spacer(),
-                Container(
-                  height: 52,
-                  width: 52,
-                  decoration: ShapeDecoration(
-                    shape: RoundedSuperellipseBorder(
-                      borderRadius: .circular(12),
-                      side: BorderSide(width: 2),
-                    ),
-                  ),
-                  child: Icon(Icons.notifications, color: Colors.black),
-                ),
-              ],
-            ),
-            Container(
-              // height: 48,
-              decoration: BoxDecoration(
-                borderRadius: .circular(12),
-                border: .all(width: 2),
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 12),
-              child: Row(
-                spacing: 12,
+                spacing: 9,
                 children: [
-                  Icon(Icons.search),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: "Search package",
-                        border: .none,
-                        hintStyle: TextStyle(fontSize: 18, fontWeight: .bold),
+                  Container(
+                    height: 52,
+                    width: 52,
+                    decoration: ShapeDecoration(
+                      color: .fromRGBO(53, 223, 104, 1),
+                      shape: RoundedSuperellipseBorder(
+                        borderRadius: .circular(12),
                       ),
                     ),
                   ),
+                  Text(
+                    "Hello, Dream!",
+                    style: TextStyle(fontSize: 22, fontWeight: .bold),
+                  ),
+                  Spacer(),
+                  Container(
+                    height: 52,
+                    width: 52,
+                    decoration: ShapeDecoration(
+                      shape: RoundedSuperellipseBorder(
+                        borderRadius: .circular(12),
+                        side: BorderSide(width: 2),
+                      ),
+                    ),
+                    child: Icon(Icons.notifications, color: Colors.black),
+                  ),
                 ],
               ),
-            ),
-
-            Expanded(
-                child: SingleChildScrollView(
-                    child: Column(
-                        spacing: 20,
-                        children: [
-                        Container(height: 280,
-                        decoration: ShapeDecoration(
-                            shape: RoundedSuperellipseBorder(
-                              borderRadius: .circular(24),
-                            ))
-                        child: Placeholder()),
-                Row(
-                  mainAxisAlignment: .spaceBetween,
-                  children: [Text("Tracking history"), Text("See all")],
+              Container(
+                // height: 48,
+                decoration: BoxDecoration(
+                  borderRadius: .circular(12),
+                  border: .all(width: 2),
                 ),
-                ...List.generate(4, (index) {
-        return Container(height: 240, child: Placeholder());
-        }),
-        ],
+                padding: EdgeInsets.symmetric(horizontal: 12),
+                child: Row(
+                  spacing: 12,
+                  children: [
+                    Icon(Icons.search),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "Search package",
+                          border: .none,
+                          hintStyle: TextStyle(fontSize: 18, fontWeight: .bold),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    spacing: 20,
+                    children: [
+                      Container(
+                        height: 280,
+                        decoration: ShapeDecoration(
+                          shape: RoundedSuperellipseBorder(
+                            borderRadius: .circular(24),
+                            side: BorderSide(width: 2),
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+
+                          ],
+                        )
+                      ),
+                      Row(
+                        mainAxisAlignment: .spaceBetween,
+                        children: [Text("Tracking history"), Text("See all")],
+                      ),
+                      ...List.generate(4, (index) {
+                        return Container(height: 240, child: Placeholder());
+                      }),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-    ),
-    ],
-    ),
-    ),
-    ),
     );
   }
 }
