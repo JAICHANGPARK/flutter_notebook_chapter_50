d833d5e353aa2958a66c6af5d1b38a431bd137fa
JAICHANGPARK <aristojeff@gmail.com>
2026-02-08 11:01:49 +0900

    Update delivery_main_page.dart

--- lib/ep2508_delivery_app/ui/delivery_main_page.dart
+++ lib/ep2508_delivery_app/ui/delivery_main_page.dart
@@ -194,7 +194,9 @@ class _DeliveryMainPageState extends State<DeliveryMainPage> {
                           ),
                         ],
                       ),
-                      ...List.generate(4, (index) {
+                      ...List.generate(4, (index)
+
+                      {
                         return Container(height: 240, child: Placeholder());
                       }),
                     ],
