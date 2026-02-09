3b5ad0316eec6f2ce7b378c1a08fae1b45732a12
JAICHANGPARK <aristojeff@gmail.com>
2026-02-09 18:48:41 +0900

    Update ep2511_main.dart

--- lib/ep2511_furniture_app/ep2511_main.dart
+++ lib/ep2511_furniture_app/ep2511_main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_50/ep2511_furniture_app/furniture_app.dart';
 
 void main() {
-  runApp(app)  
+  runApp(FurnitureApp());
 }
