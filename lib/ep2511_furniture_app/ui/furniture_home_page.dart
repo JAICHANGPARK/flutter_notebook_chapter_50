ca2ce76d873503e01301a715bdddf2525637bc73
JAICHANGPARK <aristojeff@gmail.com>
2026-02-12 19:06:46 +0900

    Update furniture_home_page.dart

--- lib/ep2511_furniture_app/ui/furniture_home_page.dart
+++ lib/ep2511_furniture_app/ui/furniture_home_page.dart
@@ -251,6 +251,7 @@ class _FurnitureHomePageState extends State<FurnitureHomePage> {
                                             top: 6,
                                             child: CircleAvatar(
                                               backgroundColor: Colors.white,
+                                              child: Icon(Icons.favorite_border),
                                             ),
                                           ),
                                         ],
