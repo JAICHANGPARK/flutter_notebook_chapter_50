e0f37e72e484c00f84647ba4e45b260c4cf975bb
JAICHANGPARK <aristojeff@gmail.com>
2026-02-12 19:05:44 +0900

    Update furniture_home_page.dart

--- lib/ep2511_furniture_app/ui/furniture_home_page.dart
+++ lib/ep2511_furniture_app/ui/furniture_home_page.dart
@@ -259,6 +259,7 @@ class _FurnitureHomePageState extends State<FurnitureHomePage> {
                                               "\$500",
                                               style: TextStyle(
                                                 fontWeight: .bold,
+                                                fontSize: 16,
                                               ),
                                             ),
                                           ],
