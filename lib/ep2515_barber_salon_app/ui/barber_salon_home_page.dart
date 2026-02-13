68f972516e6e804969e38cb608ee22778b9d3181
JAICHANGPARK <aristojeff@gmail.com>
2026-02-13 17:44:21 +0900

    Update barber_salon_home_page.dart

--- lib/ep2515_barber_salon_app/ui/barber_salon_home_page.dart
+++ lib/ep2515_barber_salon_app/ui/barber_salon_home_page.dart
@@ -11,26 +11,29 @@ class _BarberSalonHomePageState extends State<BarberSalonHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          
-        ],
-      ),
+      body: Column(children: []),
       bottomNavigationBar: BottomAppBar(
         color: Color(0xff1c1c1e),
         shape: CircularNotchedRectangle(),
         notchMargin: 8,
         clipBehavior: .antiAlias,
         height: 70,
+        child: Row(
+          children: [
+            IconButton(Icons.home_filled),
+
+          ],
+        ),
       ),
       floatingActionButtonLocation: .centerDocked,
       floatingActionButton: FloatingActionButton(
         shape: CircleBorder(),
-          elevation: 6,
-          child: Icon(Icons.add),
-         foregroundColor: Colors.white,
-          backgroundColor: Color(0xff8b5cf6),
-          onPressed: (){}),
+        elevation: 6,
+        child: Icon(Icons.add, size: 32),
+        foregroundColor: Colors.white,
+        backgroundColor: Color(0xff8b5cf6),
+        onPressed: () {},
+      ),
     );
   }
 }
