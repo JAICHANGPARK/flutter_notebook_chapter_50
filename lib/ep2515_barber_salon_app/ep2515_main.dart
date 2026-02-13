aaeff7d3feddf9afc0db161e31a552b739ec05c0
JAICHANGPARK <aristojeff@gmail.com>
2026-02-13 17:32:39 +0900

    Update ep2515_main.dart

--- lib/ep2515_barber_salon_app/ep2515_main.dart
+++ lib/ep2515_barber_salon_app/ep2515_main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_chapter_50/ep2515_barber_salon_app/barber_salon_app.dart';
 
 void main() {
-  
+  runApp(BarberSalonApp());
 }
