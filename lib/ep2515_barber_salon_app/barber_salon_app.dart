58eef50b124583656c7357bf1755d2a71dc921ca
JAICHANGPARK <aristojeff@gmail.com>
2026-02-13 17:34:14 +0900

    Update barber_salon_app.dart

--- lib/ep2515_barber_salon_app/barber_salon_app.dart
+++ lib/ep2515_barber_salon_app/barber_salon_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_50/ep2515_barber_salon_app/ui/barber_salon_home_page.dart';
 
 class BarberSalonApp extends StatelessWidget {
   const BarberSalonApp({super.key});
@@ -6,7 +7,7 @@ class BarberSalonApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: BarberSalonHomePage(),
     );
   }
 }
