0a618d702ff04d14748a99b9104a915f710670d0
JAICHANGPARK <aristojeff@gmail.com>
2026-02-13 17:31:54 +0900

    Update barber_salon_home_page.dart

--- lib/ep2515_barber_salon_app/ui/barber_salon_home_page.dart
+++ lib/ep2515_barber_salon_app/ui/barber_salon_home_page.dart
@@ -10,6 +10,10 @@ class BarberSalonHomePage extends StatefulWidget {
 class _BarberSalonHomePageState extends State<BarberSalonHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+
+      ),
+    );
   }
 }
