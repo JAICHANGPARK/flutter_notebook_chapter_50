ff42fde49935f94cbeca14afdfcec6f5fe0b3e9c
JAICHANGPARK <aristojeff@gmail.com>
2026-02-15 10:03:25 +0900

    Update barber_salon_home_page.dart

--- lib/ep2515_barber_salon_app/ui/barber_salon_home_page.dart
+++ lib/ep2515_barber_salon_app/ui/barber_salon_home_page.dart
@@ -145,12 +145,12 @@ class _BarberSalonHomePageState extends State<BarberSalonHomePage> {
                       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                         crossAxisCount: 2,
                         crossAxisSpacing: 8,
-                        mainAxisSpacing:,
+                        mainAxisSpacing:8,
                       ),
                       itemBuilder: (context, index) {
                         return Container(
                           decoration: BoxDecoration(
-                           color: Colors.blue,
+                            color: Colors.blue,
                           ),
                         );
                       },
