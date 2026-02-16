fc0d57cd2d507a334a2b5a68f51c42b572092c5a
JAICHANGPARK <aristojeff@gmail.com>
2026-02-16 10:33:02 +0900

    Update barber_salon_home_page.dart

--- lib/ep2515_barber_salon_app/ui/barber_salon_home_page.dart
+++ lib/ep2515_barber_salon_app/ui/barber_salon_home_page.dart
@@ -185,6 +185,7 @@ class _BarberSalonHomePageState extends State<BarberSalonHomePage> {
                                       ],
                                     ),
                                   ),
+                                  
                                 ],
                               ),
                             ),
