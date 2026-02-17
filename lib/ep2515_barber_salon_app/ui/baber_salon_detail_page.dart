43c996b4e70afb8f1d894da6b44a92cbe9eef8e5
JAICHANGPARK <aristojeff@gmail.com>
2026-02-18 08:44:03 +0900

    Create baber_salon_detail_page.dart

---
+++ lib/ep2515_barber_salon_app/ui/baber_salon_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BaberSalonDetailPage extends StatefulWidget {
+  const BaberSalonDetailPage({super.key});
+
+  @override
+  State<BaberSalonDetailPage> createState() => _BaberSalonDetailPageState();
+}
+
+class _BaberSalonDetailPageState extends State<BaberSalonDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
