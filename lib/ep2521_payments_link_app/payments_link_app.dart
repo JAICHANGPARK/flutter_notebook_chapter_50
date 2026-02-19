f3b9bd8a3e428412cfe84178fc41189a299766b8
JAICHANGPARK <aristojeff@gmail.com>
2026-02-19 18:43:43 +0900

    Update payments_link_app.dart

--- lib/ep2521_payments_link_app/payments_link_app.dart
+++ lib/ep2521_payments_link_app/payments_link_app.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'ui/payment_links_page.dart';
+
 
 class PaymentsLinkApp extends StatelessWidget {
   const PaymentsLinkApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: PaymentLinksPage(),
+    );
   }
 }
