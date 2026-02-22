2dfb817bd9dd9c148a7ed44d7ce8d257485acb36
JAICHANGPARK <aristojeff@gmail.com>
2026-02-22 10:04:22 +0900

    Update payment_link_generate_page.dart

--- lib/ep2521_payments_link_app/ui/payment_link_generate_page.dart
+++ lib/ep2521_payments_link_app/ui/payment_link_generate_page.dart
@@ -26,7 +26,19 @@ class _PaymentLinkGeneratePageState extends State<PaymentLinkGeneratePage> {
                       ),
                       child: Icon(Icons.clear),
                     ),
-
+                  Container(
+                    decoration: ShapeDecoration(
+                      shape: StadiumBorder(),
+                      color: Color.fromRGBO(32, 88, 154, 1),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 6,
+                    ),
+                    child: Text("Continue",style: TextStyle(
+                      color: Colors.white,
+                    ),),
+                  )
                   ],
                 ),
               ],
