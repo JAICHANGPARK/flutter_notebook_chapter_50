dc58f18aabd706b7fad4501d6a663568e792c049
JAICHANGPARK <aristojeff@gmail.com>
2026-02-22 09:55:21 +0900

    Create payment_link_generate_page.dart

---
+++ lib/ep2521_payments_link_app/ui/payment_link_generate_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PaymentLinkGeneratePage extends StatefulWidget {
+  const PaymentLinkGeneratePage({super.key});
+
+  @override
+  State<PaymentLinkGeneratePage> createState() => _PaymentLinkGeneratePageState();
+}
+
+class _PaymentLinkGeneratePageState extends State<PaymentLinkGeneratePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
