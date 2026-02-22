5e5674bf6fce5f32a8f10a193c64168c39826539
JAICHANGPARK <aristojeff@gmail.com>
2026-02-22 09:55:40 +0900

    Update payment_link_generate_page.dart

--- lib/ep2521_payments_link_app/ui/payment_link_generate_page.dart
+++ lib/ep2521_payments_link_app/ui/payment_link_generate_page.dart
@@ -10,6 +10,8 @@ class PaymentLinkGeneratePage extends StatefulWidget {
 class _PaymentLinkGeneratePageState extends State<PaymentLinkGeneratePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      
+    );
   }
 }
