f564e3a27b97e3999689adae6b3a1d51ce81add6
JAICHANGPARK <aristojeff@gmail.com>
2026-02-22 09:56:17 +0900

    Update payment_link_generate_page.dart

--- lib/ep2521_payments_link_app/ui/payment_link_generate_page.dart
+++ lib/ep2521_payments_link_app/ui/payment_link_generate_page.dart
@@ -5,7 +5,8 @@ class PaymentLinkGeneratePage extends StatefulWidget {
   const PaymentLinkGeneratePage({super.key});
 
   @override
-  State<PaymentLinkGeneratePage> createState() => _PaymentLinkGeneratePageState();
+  State<PaymentLinkGeneratePage> createState() =>
+      _PaymentLinkGeneratePageState();
 }
 
 class _PaymentLinkGeneratePageState extends State<PaymentLinkGeneratePage> {
@@ -13,58 +14,58 @@ class _PaymentLinkGeneratePageState extends State<PaymentLinkGeneratePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-children: [
-  Positioned(
-    bottom: 42,
-    left: 32,
-    right: 32,
-    child: Container(
-      padding: EdgeInsets.all(16),
-      decoration: ShapeDecoration(
-        shape: RoundedSuperellipseBorder(
-          borderRadius: BorderRadius.circular(32),
-        ),
-        color: Colors.white,
-        shadows: [
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 5,
-            spreadRadius: 5,
-          ),
-        ],
-      ),
-      child: Column(
-        spacing: 16,
         children: [
-          Container(
-            decoration: ShapeDecoration(
-              shape: StadiumBorder(),
-              color: Colors.grey[100]!,
+          Positioned(
+            bottom: 42,
+            left: 32,
+            right: 32,
+            child: Container(
+              padding: EdgeInsets.all(16),
+              decoration: ShapeDecoration(
+                shape: RoundedSuperellipseBorder(
+                  borderRadius: BorderRadius.circular(32),
+                ),
+                color: Colors.white,
+                shadows: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 5,
+                    spreadRadius: 5,
+                  ),
+                ],
+              ),
+              child: Column(
+                spacing: 16,
+                children: [
+                  Container(
+                    decoration: ShapeDecoration(
+                      shape: StadiumBorder(),
+                      color: Colors.grey[100]!,
+                    ),
+                    padding: EdgeInsets.all(8),
+                    child: Row(
+                      children: [
+                        Icon(CupertinoIcons.list_bullet_below_rectangle),
+                        Expanded(child: Center(child: Text("dreamwalker."))),
+                        Icon(CupertinoIcons.refresh),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(CupertinoIcons.back),
+                      Icon(CupertinoIcons.forward),
+                      Icon(CupertinoIcons.share),
+                      Icon(CupertinoIcons.book),
+                      Icon(CupertinoIcons.doc_on_clipboard),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            padding: EdgeInsets.all(8),
-            child: Row(
-              children: [
-                Icon(CupertinoIcons.list_bullet_below_rectangle),
-                Expanded(child: Center(child: Text("dreamwalker."))),
-                Icon(CupertinoIcons.refresh),
-              ],
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Icon(CupertinoIcons.back),
-              Icon(CupertinoIcons.forward),
-              Icon(CupertinoIcons.share),
-              Icon(CupertinoIcons.book),
-              Icon(CupertinoIcons.doc_on_clipboard),
-            ],
           ),
         ],
-      ),
-    ),
-  ),
-],
       ),
     );
   }
