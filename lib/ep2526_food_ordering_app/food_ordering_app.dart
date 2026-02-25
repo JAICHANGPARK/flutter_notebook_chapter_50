59ca7e43801e61444238094af68a54ab31be0cad
JAICHANGPARK <aristojeff@gmail.com>
2026-02-25 22:53:54 +0900

    Create food_ordering_app.dart

---
+++ lib/ep2526_food_ordering_app/food_ordering_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class FoodOrderingApp extends StatelessWidget {
+  const FoodOrderingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
