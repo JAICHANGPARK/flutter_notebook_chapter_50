89054902d5c3df7fbfb696c4683d725b05b720f6
JAICHANGPARK <aristojeff@gmail.com>
2026-02-25 22:56:47 +0900

    Update food_ordering_main_page.dart

--- lib/ep2526_food_ordering_app/ui/food_ordering_main_page.dart
+++ lib/ep2526_food_ordering_app/ui/food_ordering_main_page.dart
@@ -10,6 +10,12 @@ class FoodOrderingMainPage extends StatefulWidget {
 class _FoodOrderingMainPageState extends State<FoodOrderingMainPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
   }
 }
