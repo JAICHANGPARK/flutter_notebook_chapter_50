bbb2b7ce36cc85892d4db2be794f96e57723a50a
JAICHANGPARK <aristojeff@gmail.com>
2026-02-25 22:55:38 +0900

    Create food_ordering_main_page.dart

---
+++ lib/ep2526_food_ordering_app/ui/food_ordering_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FoodOrderingMainPage extends StatefulWidget {
+  const FoodOrderingMainPage({super.key});
+
+  @override
+  State<FoodOrderingMainPage> createState() => _FoodOrderingMainPageState();
+}
+
+class _FoodOrderingMainPageState extends State<FoodOrderingMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
