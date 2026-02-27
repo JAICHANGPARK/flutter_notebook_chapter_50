5ef9f8c3b0cec06b9bbde46de41565d6f423d8f4
JAICHANGPARK <aristojeff@gmail.com>
2026-02-27 22:57:17 +0900

    Update food_ordering_main_page.dart

--- lib/ep2526_food_ordering_app/ui/food_ordering_main_page.dart
+++ lib/ep2526_food_ordering_app/ui/food_ordering_main_page.dart
@@ -32,16 +32,18 @@ class _FoodOrderingMainPageState extends State<FoodOrderingMainPage> {
               child: Row(
                 mainAxisAlignment: .spaceAround,
                 children: [
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        pageNum = 0;
-                      });
-                    },
-                    child: Column(
-                      spacing: 4,
-                      mainAxisAlignment: .center,
-                      children: [Icon(Icons.home_filled), Text("Home")],
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          pageNum = 0;
+                        });
+                      },
+                      child: Column(
+                        spacing: 4,
+                        mainAxisAlignment: .center,
+                        children: [Icon(Icons.home_filled), Text("Home")],
+                      ),
                     ),
                   ),
                   GestureDetector(
