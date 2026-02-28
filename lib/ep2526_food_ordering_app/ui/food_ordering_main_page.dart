8e1a4260fefa82d303f807efad6f07fc1e253116
JAICHANGPARK <aristojeff@gmail.com>
2026-02-28 19:40:01 +0900

    Update food_ordering_main_page.dart

--- lib/ep2526_food_ordering_app/ui/food_ordering_main_page.dart
+++ lib/ep2526_food_ordering_app/ui/food_ordering_main_page.dart
@@ -31,9 +31,10 @@ class _FoodOrderingMainPageState extends State<FoodOrderingMainPage> {
                         height: 140,
                         decoration: BoxDecoration(
                           gradient: LinearGradient(
-                            begin: .topRight,
-                            end: .centerRight,
+                            begin: .centerLeft,
+                            end: .bottomRight,
                             colors: [
+                              Color.fromRGBO(232, 95, 16, 1),
                               Color.fromRGBO(232, 95, 16, 1),
                               Color.fromRGBO(142, 59, 9, 1),
                             ],
