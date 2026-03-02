7c57b44fa776c9276a320a6ae7ef1786e2d93bce
JAICHANGPARK <aristojeff@gmail.com>
2026-03-02 10:17:51 +0900

    Update wellness_main_page.dart

---
+++ lib/ep2530_wellness_app/ui/wellness_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class WellnessMainPage extends StatefulWidget {
+  const WellnessMainPage({super.key});
+
+  @override
+  State<WellnessMainPage> createState() => _WellnessMainPageState();
+}
+
+class _WellnessMainPageState extends State<WellnessMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
