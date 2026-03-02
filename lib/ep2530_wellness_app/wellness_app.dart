a63edd8899386e33ccffd041d5e70b4b0e808aeb
JAICHANGPARK <aristojeff@gmail.com>
2026-03-02 10:17:49 +0900

    Update wellness_app.dart

--- lib/ep2530_wellness_app/wellness_app.dart
+++ lib/ep2530_wellness_app/wellness_app.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'ui/wellness_main_page.dart';
+
 class WellnessApp extends StatelessWidget {
   const WellnessApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: WellnessMainPage(),
+    );
   }
 }
