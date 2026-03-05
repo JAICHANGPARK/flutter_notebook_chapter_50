d0e6a598038b9a8262f890a06db76ce2a7f3320f
JAICHANGPARK <aristojeff@gmail.com>
2026-03-05 18:42:01 +0900

    Update fashion_mobile_app.dart

--- lib/ep2533_fashion_ecommerce_app/fashion_mobile_app.dart
+++ lib/ep2533_fashion_ecommerce_app/fashion_mobile_app.dart
@@ -6,6 +6,8 @@ class FashionMobileApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      
+    );
   }
 }
