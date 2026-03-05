824b820516fe39ad636e2dbf80c193c04646887e
JAICHANGPARK <aristojeff@gmail.com>
2026-03-05 18:47:37 +0900

    Update fashion_main_page.dart

--- lib/ep2533_fashion_ecommerce_app/ui/fashion_main_page.dart
+++ lib/ep2533_fashion_ecommerce_app/ui/fashion_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FashionMainPage extends StatefulWidget {
   const FashionMainPage({super.key});
 
@@ -14,7 +13,17 @@ class _FashionMainPageState extends State<FashionMainPage> {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       body: Stack(
-
+        children: [
+          Positioned.fill(child: Column()),
+          Positioned(
+            child: Container(
+              decoration: ShapeDecoration(
+                shape: StadiumBorder(),
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
