c999f37de4a0da19b6cf224a383bf3384074d18a
JAICHANGPARK <aristojeff@gmail.com>
2026-03-05 18:51:06 +0900

    Update fashion_main_page.dart

--- lib/ep2533_fashion_ecommerce_app/ui/fashion_main_page.dart
+++ lib/ep2533_fashion_ecommerce_app/ui/fashion_main_page.dart
@@ -19,23 +19,36 @@ class _FashionMainPageState extends State<FashionMainPage> {
             bottom: 16,
             left: 16,
             right: 16,
-            child: Container(
-              decoration: ShapeDecoration(
-                shape: StadiumBorder(),
-                color: Colors.black,
-              ),
-              child: Row(
-                children: [
-                  Container(
-                    height: 64,
-                    width: 64,
-                    decoration: BoxDecoration(
-                      shape: .circle,
-
-                      color: Colors.white,
+            child: Align(
+              alignment: .center,
+              child: Container(
+                decoration: ShapeDecoration(
+                  shape: StadiumBorder(),
+                  color: Colors.black,
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      height: 64,
+                      width: 64,
+                      decoration: BoxDecoration(
+                        shape: .circle,
+                        color: Colors.white,
+                      ),
+                      child: Center(child: Icon(Icons.home_filled)),
                     ),
-                  ),
-                ],
+                    Container(
+                      height: 64,
+                      width: 64,
+                      decoration: BoxDecoration(
+                        shape: .circle,
+                        border: .all(color: Colors.white24),
+                      ),
+                      child: Center(child: Icon(Icons.home_filled)),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
