48b8e088bd4c66b81e35716ed1b595f140f16989
JAICHANGPARK <aristojeff@gmail.com>
2026-03-09 17:57:44 +0900

    Create fashion_search_page.dart

---
+++ lib/ep2533_fashion_ecommerce_app/ui/fashion_search_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FashionSearchPage extends StatefulWidget {
+  const FashionSearchPage({super.key});
+
+  @override
+  State<FashionSearchPage> createState() => _FashionSearchPageState();
+}
+
+class _FashionSearchPageState extends State<FashionSearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
