ee1bebcc4adbd7c85ae1c89baa26f2f915be0fb4
JAICHANGPARK <aristojeff@gmail.com>
2026-03-09 18:01:25 +0900

    Update fashion_search_page.dart

--- lib/ep2533_fashion_ecommerce_app/ui/fashion_search_page.dart
+++ lib/ep2533_fashion_ecommerce_app/ui/fashion_search_page.dart
@@ -13,7 +13,6 @@ class _FashionSearchPageState extends State<FashionSearchPage> {
     return SafeArea(
       bottom: false,
       child: Padding(
-
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
           spacing: 20,
@@ -22,7 +21,9 @@ class _FashionSearchPageState extends State<FashionSearchPage> {
             SizedBox(height: 80, child: Placeholder()),
 
             SizedBox(height: 52, child: Placeholder()),
-            Expanded(child: ListView.builder(itemBuilder: (context,index){},)),
+            Expanded(child: ListView.builder(itemBuilder: (context, index) {
+              return Placeholder();
+            })),
           ],
         ),
       ),
