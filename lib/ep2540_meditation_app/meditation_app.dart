10d10fc1875d880db8d51ce99bb38eb742826bc7
JAICHANGPARK <aristojeff@gmail.com>
2026-03-13 09:52:26 +0900

    Update meditation_app.dart

---
+++ lib/ep2540_meditation_app/meditation_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class MeditationApp extends StatelessWidget {
+  const MeditationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
