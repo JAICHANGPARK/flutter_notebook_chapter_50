b381a44434989621f4efaefff46ef9e890365158
JAICHANGPARK <aristojeff@gmail.com>
2026-03-13 09:53:58 +0900

    Create meditation_main_page.dart

---
+++ lib/ep2540_meditation_app/ui/meditation_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MeditationMainPage extends StatefulWidget {
+  const MeditationMainPage({super.key});
+
+  @override
+  State<MeditationMainPage> createState() => _MeditationMainPageState();
+}
+
+class _MeditationMainPageState extends State<MeditationMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
