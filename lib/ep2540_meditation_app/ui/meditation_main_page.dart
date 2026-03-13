2cde406708c574791ed09fe36d1781bd02cc97ff
JAICHANGPARK <aristojeff@gmail.com>
2026-03-13 09:59:52 +0900

    Update meditation_main_page.dart

--- lib/ep2540_meditation_app/ui/meditation_main_page.dart
+++ lib/ep2540_meditation_app/ui/meditation_main_page.dart
@@ -13,22 +13,35 @@ class _MeditationMainPageState extends State<MeditationMainPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                CircleAvatar(),
-                Expanded(child: Column(children: [])),
-                Container(
-                  height: 42,
-                  width: 42,
-                  decoration: BoxDecoration(
-                    color: Colors.blue
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                spacing: 8,
+                children: [
+                  CircleAvatar(
+                    radius: 30,
                   ),
-                ),
-              ],
-            ),
-          ],
+                  Expanded(child: Column(children: [])),
+                  Container(
+                    height: 52,
+                    width: 52,
+                    decoration: BoxDecoration(
+                      color: Colors.blue
+                    ),
+                  ),
+                  Container(
+                    height: 52,
+                    width: 52,
+                    decoration: BoxDecoration(
+                        color: Colors.blue
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
