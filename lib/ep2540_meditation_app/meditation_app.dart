c2eaec9828f9923b3d19e8b72c4999538704af63
JAICHANGPARK <aristojeff@gmail.com>
2026-03-13 09:55:42 +0900

    Update meditation_app.dart

--- lib/ep2540_meditation_app/meditation_app.dart
+++ lib/ep2540_meditation_app/meditation_app.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'ui/meditation_main_page.dart';
+
 class MeditationApp extends StatelessWidget {
   const MeditationApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: MeditationMainPage(),
+    );
   }
 }
