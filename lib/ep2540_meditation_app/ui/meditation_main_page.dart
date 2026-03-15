74e74cc09cd87b952e45b4ec793a18fb78722457
JAICHANGPARK <aristojeff@gmail.com>
2026-03-15 09:41:12 +0900

    Update meditation_main_page.dart

--- lib/ep2540_meditation_app/ui/meditation_main_page.dart
+++ lib/ep2540_meditation_app/ui/meditation_main_page.dart
@@ -164,7 +164,6 @@ class _MeditationMainPageState extends State<MeditationMainPage> {
                                 decoration: ShapeDecoration(
                                   shape: RoundedSuperellipseBorder(
                                     borderRadius: .circular(24),
-
                                   ),
                                   gradient: LinearGradient(
                                     colors: [
@@ -174,7 +173,6 @@ class _MeditationMainPageState extends State<MeditationMainPage> {
                                     ],
                                   ),
                                 ),
-
                               );
                             }),
                           ],
@@ -188,50 +186,54 @@ class _MeditationMainPageState extends State<MeditationMainPage> {
           ),
         ),
       ),
-      bottomNavigationBar: BottomAppBar(child: Row(children: [
-        Container(
-          decoration: BoxDecoration(
-            shape: .circle,
-            gradient: LinearGradient(
-              colors: [
-                Color.fromRGBO(217, 230, 101, 1),
-                Color.fromRGBO(227, 220, 165, 1),
-                Color.fromRGBO(243, 208, 235, 1),
-              ],
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                shape: .circle,
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromRGBO(217, 230, 101, 1),
+                    Color.fromRGBO(227, 220, 165, 1),
+                    Color.fromRGBO(243, 208, 235, 1),
+                  ],
+                ),
+              ),
+              padding: EdgeInsets.all(16),
+              child: HugeIcon(icon: HugeIcons.strokeRoundedHome01),
             ),
-          ),
-          padding: EdgeInsets.all(16),
-          child: HugeIcon(icon: HugeIcons.strokeRoundedHome01),
+            Container(
+              decoration: BoxDecoration(
+                shape: .circle,
+                // gradient: LinearGradient(
+                //   colors: [
+                //     Color.fromRGBO(217, 230, 101, 1),
+                //     Color.fromRGBO(227, 220, 165, 1),
+                //     Color.fromRGBO(243, 208, 235, 1),
+                //   ],
+                // ),
+              ),
+              padding: EdgeInsets.all(16),
+              child: HugeIcon(icon: HugeIcons.strokeRoundedBulb),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                shape: .circle,
+                // gradient: LinearGradient(
+                //   colors: [
+                //     Color.fromRGBO(217, 230, 101, 1),
+                //     Color.fromRGBO(227, 220, 165, 1),
+                //     Color.fromRGBO(243, 208, 235, 1),
+                //   ],
+                // ),
+              ),
+              padding: EdgeInsets.all(16),
+              child: HugeIcon(icon: HugeIcons.strokeRoundedBrain),
+            ),
+          ],
         ),
-        Container(
-          decoration: BoxDecoration(
-            shape: .circle,
-            // gradient: LinearGradient(
-            //   colors: [
-            //     Color.fromRGBO(217, 230, 101, 1),
-            //     Color.fromRGBO(227, 220, 165, 1),
-            //     Color.fromRGBO(243, 208, 235, 1),
-            //   ],
-            // ),
-          ),
-          padding: EdgeInsets.all(16),
-          child: HugeIcon(icon: HugeIcons.strokeRoundedBulb),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            shape: .circle,
-            // gradient: LinearGradient(
-            //   colors: [
-            //     Color.fromRGBO(217, 230, 101, 1),
-            //     Color.fromRGBO(227, 220, 165, 1),
-            //     Color.fromRGBO(243, 208, 235, 1),
-            //   ],
-            // ),
-          ),
-          padding: EdgeInsets.all(16),
-          child: HugeIcon(icon: HugeIcons.strokeRoundedBrain),
-        )
-      ])),
+      ),
     );
   }
 }
