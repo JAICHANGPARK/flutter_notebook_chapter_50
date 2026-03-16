076fc47913c3f3a65b5094e1e847d84b716b801c
JAICHANGPARK <aristojeff@gmail.com>
2026-03-16 17:10:37 +0900

    Update meditation_meditate_page.dart

--- lib/ep2540_meditation_app/ui/meditation_meditate_page.dart
+++ lib/ep2540_meditation_app/ui/meditation_meditate_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hugeicons/hugeicons.dart';
 
 class MeditationMeditatePage extends StatefulWidget {
   const MeditationMeditatePage({super.key});
@@ -11,10 +12,46 @@ class _MeditationMeditatePageState extends State<MeditationMeditatePage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(children: [
-      
-      ],),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              spacing: 8,
+              children: [
+                Expanded(
+                  child: Text(
+                    "Meditate",
+                    style: TextStyle(fontSize: 28, fontWeight: .bold),
+                  ),
+                ),
+                Container(
+                  height: 54,
+                  width: 54,
+                  decoration: BoxDecoration(
+                    border: .all(color: Colors.grey[200]!),
+                    shape: .circle,
+                  ),
+                  child: Center(
+                    child: HugeIcon(icon: HugeIcons.strokeRoundedCrown),
+                  ),
+                ),
+                Container(
+                  height: 54,
+                  width: 54,
+                  decoration: BoxDecoration(
+                    border: .all(color: Colors.grey[200]!),
+                    shape: .circle,
+                  ),
+                  child: Center(
+                    child: HugeIcon(icon: HugeIcons.strokeRoundedNotification01),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
-
   }
 }
