262c2c0ce62ffdf8969e7e54b187f21f0bbb0727
JAICHANGPARK <aristojeff@gmail.com>
2026-03-16 17:11:31 +0900

    Update meditation_home_page.dart

--- lib/ep2540_meditation_app/ui/meditation_home_page.dart
+++ lib/ep2540_meditation_app/ui/meditation_home_page.dart
@@ -60,43 +60,7 @@ class _MeditationHomePageState extends State<MeditationHomePage> {
                 ),
               ],
             ),
-            Container(
-              height: 54,
-              padding: EdgeInsets.only(left: 16),
-              decoration: ShapeDecoration(
-                shape: StadiumBorder(),
-                color: Colors.grey[100]!,
-              ),
-              child: Row(
-                spacing: 12,
-                children: [
-                  Icon(Icons.search),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: .none,
-                        hintText: "Search",
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: 54,
-                    width: 54,
-                    decoration: BoxDecoration(
-                      shape: .circle,
-                      gradient: LinearGradient(
-                        colors: [
-                          Color.fromRGBO(217, 230, 101, 1),
-                          Color.fromRGBO(227, 220, 165, 1),
-                          Color.fromRGBO(243, 208, 235, 1),
-                        ],
-                      ),
-                    ),
-                    child: Icon(Icons.tune),
-                  ),
-                ],
-              ),
-            ),
+            MeditationSearchBar(),
             Expanded(
               child: SingleChildScrollView(
                 child: Column(
