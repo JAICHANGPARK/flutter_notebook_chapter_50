385ae7463a1c50ac3e1769b38921e98b50140b2f
JAICHANGPARK <aristojeff@gmail.com>
2026-03-16 17:05:37 +0900

    Update meditation_meditate_page.dart

--- lib/ep2540_meditation_app/ui/meditation_meditate_page.dart
+++ lib/ep2540_meditation_app/ui/meditation_meditate_page.dart
@@ -1,2 +1,18 @@
 import 'package:flutter/material.dart';
 
+class MeditationMeditatePage extends StatefulWidget {
+  const MeditationMeditatePage({super.key});
+
+  @override
+  State<MeditationMeditatePage> createState() => _MeditationMeditatePageState();
+}
+
+class _MeditationMeditatePageState extends State<MeditationMeditatePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+
+    ],);
+
+  }
+}
