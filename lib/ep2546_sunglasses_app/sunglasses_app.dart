55b596fdc3f31dc797d0c5ff8e635b06ae61c477
JAICHANGPARK <aristojeff@gmail.com>
2026-03-21 09:56:24 +0900

    Create sunglasses_app.dart

---
+++ lib/ep2546_sunglasses_app/sunglasses_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class SunglassesApp extends StatelessWidget {
+  const SunglassesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
