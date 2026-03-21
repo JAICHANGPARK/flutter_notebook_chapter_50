cc0cef9ba665c80f83cc5f59cfecd788e3996239
JAICHANGPARK <aristojeff@gmail.com>
2026-03-21 09:57:13 +0900

    Update sunglasses_cart_page.dart

---
+++ lib/ep2546_sunglasses_app/ui/sunglasses_cart_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SunglassesCartPage extends StatefulWidget {
+  const SunglassesCartPage({super.key});
+
+  @override
+  State<SunglassesCartPage> createState() => _SunglassesCartPageState();
+}
+
+class _SunglassesCartPageState extends State<SunglassesCartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
