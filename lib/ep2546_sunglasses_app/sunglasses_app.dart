4f2bc66715dcca0cd52196bc464513889f64c66a
JAICHANGPARK <aristojeff@gmail.com>
2026-03-22 09:25:00 +0900

    Update sunglasses_app.dart

--- lib/ep2546_sunglasses_app/sunglasses_app.dart
+++ lib/ep2546_sunglasses_app/sunglasses_app.dart
@@ -2,14 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'ui/sunglasses_cart_page.dart';
 
-
 class SunglassesApp extends StatelessWidget {
   const SunglassesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SunglassesCartPage(),
-    );
+    return MaterialApp(home: SunglassesCartPage());
   }
 }
