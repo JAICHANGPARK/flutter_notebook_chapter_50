c89da02cb0815b509ae5b61660488f80e8615968
JAICHANGPARK <aristojeff@gmail.com>
2026-03-24 18:40:18 +0900

    Create sunglasses_home_page.dart

---
+++ lib/ep2546_sunglasses_app/ui/sunglasses_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SunglassesHomePage extends StatefulWidget {
+  const SunglassesHomePage({super.key});
+
+  @override
+  State<SunglassesHomePage> createState() => _SunglassesHomePageState();
+}
+
+class _SunglassesHomePageState extends State<SunglassesHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
